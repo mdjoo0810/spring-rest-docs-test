2725aa50025953ca72e99ed14dbe2430885dec74
laon-private <71617071+laon-private@users.noreply.github.com>
2021-04-29 09:31:39 +0900

    [feat] add User Entity

---
+++ src/main/java/kr/springrestdocstest/model/User.java
@@ -0,0 +1,30 @@
+package kr.springrestdocstest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+}
