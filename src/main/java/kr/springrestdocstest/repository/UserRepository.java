af25a342de167b9efc42f069517cfb32ecf58e43
laon-private <71617071+laon-private@users.noreply.github.com>
2021-04-29 09:31:45 +0900

    [feat] add User Repository

---
+++ src/main/java/kr/springrestdocstest/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package kr.springrestdocstest.repository;
+
+import kr.springrestdocstest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
