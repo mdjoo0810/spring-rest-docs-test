a274e2635afe8a6f1988286df26513871158b8c9
laon-private <71617071+laon-private@users.noreply.github.com>
2021-04-29 09:31:32 +0900

    [feat] first commit

---
+++ src/main/java/kr/springrestdocstest/SpringRestDocsTestApplication.java
@@ -0,0 +1,13 @@
+package kr.springrestdocstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/springrestdocstest/SpringRestDocsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.springrestdocstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
